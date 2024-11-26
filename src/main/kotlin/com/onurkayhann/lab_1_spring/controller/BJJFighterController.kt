fb8d351fe2f5dd45725cef789d83d67994789012
Onur <onur.kayhan90@gmail.com>
2024-11-26 18:19:07 +0100

    Feature: Add RequestMapping

--- src/main/kotlin/com/onurkayhann/lab_1_spring/controller/BJJFighterController.kt
+++ src/main/kotlin/com/onurkayhann/lab_1_spring/controller/BJJFighterController.kt
@@ -5,14 +5,16 @@ import com.onurkayhann.lab_1_spring.repository.BJJFighterRepository
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/fighters")
 class BJJFighterController(
     private val bjjFighterRepository: BJJFighterRepository
 ) {
 
-    @PostMapping("/fighters")
+    @PostMapping
     fun addNewFighter(@RequestBody bjjFighter: BJJFighter): ResponseEntity<String> {
 
         bjjFighterRepository.save(bjjFighter)
