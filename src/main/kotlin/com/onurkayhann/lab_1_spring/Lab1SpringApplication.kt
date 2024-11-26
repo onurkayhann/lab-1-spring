d622b652f35feda51964e7ccd1a33b89009987b8
Onur <onur.kayhan90@gmail.com>
2024-11-26 17:30:34 +0100

    Initial commit

---
+++ src/main/kotlin/com/onurkayhann/lab_1_spring/Lab1SpringApplication.kt
@@ -0,0 +1,11 @@
+package com.onurkayhann.lab_1_spring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Lab1SpringApplication
+
+fun main(args: Array<String>) {
+	runApplication<Lab1SpringApplication>(*args)
+}
---
+++ src/test/kotlin/com/onurkayhann/lab_1_spring/Lab1SpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.onurkayhann.lab_1_spring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Lab1SpringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
