57468e7a07ab2fca202cbb27a793121810e61d96
Onur <onur.kayhan90@gmail.com>
2024-11-26 17:57:53 +0100

    Feature: Add RestController, BJJFighterRepository and Postmapping in the Controller

--- src/main/kotlin/com/onurkayhann/lab_1_spring/controller/BJJFighterController.kt
+++ src/main/kotlin/com/onurkayhann/lab_1_spring/controller/BJJFighterController.kt
@@ -1,4 +1,21 @@
 package com.onurkayhann.lab_1_spring.controller
 
-class BJJFighterController {
+import com.onurkayhann.lab_1_spring.model.BJJFighter
+import com.onurkayhann.lab_1_spring.repository.BJJFighterRepository
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class BJJFighterController(
+    private val bjjFighterRepository: BJJFighterRepository
+) {
+
+    @PostMapping
+    fun addNewFighter(@RequestBody bjjFighter: BJJFighter): BJJFighter {
+
+        return bjjFighterRepository.save(bjjFighter)
+
+    }
+
 }
